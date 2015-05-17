87661d9466f6f670c6438dd452cc88c02c25ce3f
Ariana <dorado_ariana@yahoo.com>
2015-05-17 12:23:36 -0600

    Added ability to destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-    redirect_to @article
+      redirect_to @article
     else
       render 'new'
     end
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
